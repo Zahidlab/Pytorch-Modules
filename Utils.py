6f388c2078eae7a9ec40ac50f118ab19afa03907
Zahid <38046594+Zahidlab@users.noreply.github.com>
2023-02-23 13:46:23 +0600

    Update Utils.py

--- Utils.py
+++ Utils.py
@@ -13,3 +13,34 @@ def show_batch(dl, nmax=64, torch_tensor = True):
       denormalize(images.detach()[:nmax]), nrow = 8).permute(1,2,0)
       
   )
+  
+  
+  def get_name(t, acc = False, model = "CNN"):
+    
+    root_path = r"C:\Users\HP\Documents\Zahid\OCT\\"
+    
+    model_save_path =os.path.join(root_path, "Models")
+    log_save_path = os.path.join(root_path, "Logs")
+    graph_save_path = os.path.join(root_path, "Graphs")
+    results_save_path = os.path.join(root_path, "Results")
+    
+    project_name = "OCT Classifier"
+    
+    
+    if t=="graph":
+        if acc:
+            uni_name = os.path.join(graph_save_path, f"{model} {project_name} ACC {get_date_time()}.jpg")
+        else:
+            uni_name = os.path.join(graph_save_path, f"{model} {project_name} {get_date_time()}.jpg")
+            
+    elif t=="model":
+        uni_name = os.path.join(model_save_path, f"{model} {project_name} {get_date_time()}.pth")
+    elif t=='log':
+        uni_name = os.path.join(log_save_path, f"{model} {project_name} {get_date_time()}.csv")
+    elif t== 'results':
+        uni_name = os.path.join(results_save_path, f"{model} {project_name} {get_date_time()}.csv")
+    else:
+        raise ValueError(f'You entered invalid value')
+        
+        
+    return uni_name
